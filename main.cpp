226dd215d6a3f25ae886194d2eb1090f4de738ec
Crocussys <egor.vladislavovich.nn@gmail.com>
2021-05-14 14:32:10 +0300

    Main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
-#include <QCoreApplication>
+#include <iostream>
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
-// 123132
-    return a.exec();
+    if(argc < 3){
+        cout << "Недостаточно аргументов: " << argc << " из 3" << endl;
+    }
+    return 0;
 }
