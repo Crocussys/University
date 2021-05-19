06d82492747013068c07a3bf9bef7353e3c5e35c
leha123456789 <leha.denisov.03@mail.ru>
2021-05-19 15:24:38 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
+    Teacher ms;
+    int k =100;
+    Neacher *Groups;
+    Groups=new Teacher[k];
     if(argc < 3){
         cout << "Недостаточно аргументов: " << argc << " из 3" << endl;
     }
