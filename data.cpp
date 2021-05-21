28d065f3fdd9b5f7d6850000e4212860177f7ee3
cuber201 <72391128+cuber201@users.noreply.github.com>
2021-05-21 20:10:33 +0300

    Create data.cpp

--- data.cpp
+++ data.cpp
@@ -2,41 +2,33 @@
 
 Data::Data()
 {
-    Teacher* teachers;
-    fstream teachers_file;
-    char* teachers_file_name;
-    int teachers_file_size;
 
-    Student* students;
-    fstream students_file;
-    char* students_file_name;
-    int students_file_size;
 }
 void Data::TeacherLoad(char*filename){
     file >> size;
-    Teach = new Teacher[size];
+    Teachers = new Teacher[size];
     for (int i = 0; i < size; i ++){
-        file >> Teach[i];
+        file >> Teachers[i];
     }
 void Data::StudentLoad(char*filename){
     file >> size;
     Stud = new Student[size];
     for (int i = 0; i < size; i ++){
-        file >> Stud[i];
+        file >> Students[i];
     }
 void Data::TeacherSave(char*filename){
     file.close();
     file.open(fileName);
     file << size;
     for (int i = 0; i < size; i++){
-        file << Teach[i];
+        file << Teachers[i];
     }
 void Data::StudentSave(char*filename){
     file.close();
     file.open(fileName);
     file << size;
     for (int i = 0; i < size; i++){
-        file << Srud[i];
+        file << Studens[i];
     }
     file.close();
     file.open(fileName);
