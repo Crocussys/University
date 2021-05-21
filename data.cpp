e617a7110a665f9d9c301fdbe28b8c6170752ff6
leha123456789 <leha.denisov.03@mail.ru>
2021-05-21 19:53:30 +0300

    Update data.cpp

--- data.cpp
+++ data.cpp
@@ -4,6 +4,12 @@ Data::Data()
 {
 
 }
+void DataBase::Load(){
+    file >> size;
+    main = new Product[size];
+    for (int i = 0; i < size; i ++){
+        file >> main[i];
+    }
 void Data::TeacherSave(){
     file.close();
     file.open(FileName);
