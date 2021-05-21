bfc9f527ba912bc1b402288225ba06d8fbda77fb
leha123456789 <leha.denisov.03@mail.ru>
2021-05-21 20:11:41 +0300

    Update data.h

--- data.h
+++ data.h
@@ -4,6 +4,16 @@
 
 class Data
 {
+private:
+    Teacher* teachers;
+    fstream teachers_file;
+    char* teachers_file_name;
+    int teachers_file_size;
+
+    Student* students;
+    fstream students_file;
+    char* students_file_name;
+    int students_file_size;
 public:
     Data();
       void TeacherLoad();
