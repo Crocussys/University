5bad7123d5a8719647fb7e583c3965b4c70af0d8
leha123456789 <leha.denisov.03@mail.ru>
2021-05-21 19:43:37 +0300

    Update data.cpp

--- data.cpp
+++ data.cpp
@@ -6,11 +6,11 @@ Data::Data()
 }
 void DataBase::Save(){
     file.close();
-    file.open(FileName, ios::binary|ofstream::out|ofstream::trunc);
+    file.open(FileName);
     file << size;
     for (int i = 0; i < size; i++){
         file << main[i];
     }
     file.close();
-    file.open(FileName, ios::binary|ios::in);
+    file.open(FileName);
 }
